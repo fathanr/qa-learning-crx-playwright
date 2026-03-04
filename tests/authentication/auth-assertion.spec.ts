dbe2b834eb79b0a8892ad07b3e6af2e69382eb85
hanaaaca <hana@dot-indonesia.com>
2026-03-04 16:14:32 +0700

    auth first record with assertion

---
+++ tests/authentication/auth-assertion.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    //Go to link website
+  await page.goto('https://the-internet.herokuapp.com/login');
+  // Input username
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
+  // Input password
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  // Klik tombol Login
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  //assertion warning message success login
+  await expect(page.locator('#flash')).toContainText('You logged into a secure area');
+
+  //assertion element log out
+  await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
+});
