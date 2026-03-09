e9f5cd7816b51ab16239bf659326c3d52de0256c
hancapr4 <hana@dot-indonesia.com>
2026-03-09 09:19:33 +0700

    filter todo

--- tests/TodoTest/FilterTodo.spec.js
+++ tests/TodoTest/FilterTodo.spec.js
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { CreateTodo } from '../../pages/classes/Create';
-import { DeleteTodo } from '../../pages/classes/DeleteTodo';
+import { CheckTodo } from '../../pages/classes/CheckTodo';
 import { LocatorMaster } from '../../pages/locators/TodoLocators';
 
-test.describe('Delete Todo', () => {
+test.describe('Filter Todo', () => {
 
-  test('Delete first todo', async ({ page }) => {
-    const deleteTodo = new DeleteTodo(page);
+  test('Filter active and completed todo', async ({ page }) => {
+    const check = new CheckTodo(page);
     const create = new CreateTodo(page);
     const locator = new LocatorMaster(page);
     // navigate to URL
@@ -21,20 +21,31 @@ test.describe('Delete Todo', () => {
 
     // input todo
     await create.createTodo('Todo 1');
+    await create.createTodo('Todo 2');
+    await create.createTodo('Todo 3');
 
     // after input the todo please wait for 3 seconds
     await page.waitForTimeout(3000);
 
-    // assert inputed todo
+    // check first top todo
+    await check.checkTodo(0);
+
+    //filter active todo
+    await locator.filterActive.click();
+    await page.waitForTimeout(3000);
+
+    // assert filter result
+    await expect(locator.todoItem(0)).toContainText('Todo 2');
+
+    //filter completed todo
+    await locator.filterCompleted.click()
+    await page.waitForTimeout(3000);
+
+    // assert filter result
     await expect(locator.todoItem(0)).toContainText('Todo 1');
-
-    // delete first todo on top list
-    await deleteTodo.deleteTodo(0);
-
-    // assert deleted todo not visible
-    await expect(locator.todoItem(0)).not.toBeVisible();
 
     // wait for 3 seconds for end test
     await page.waitForTimeout(3000);
   });
+
 })
