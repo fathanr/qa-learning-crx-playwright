51af15626f4b0c788a3f22c7c51ea4256efade7f
hancapr4 <hana@dot-indonesia.com>
2026-03-09 08:31:35 +0700

    check todo

---
+++ pages/classes/CheckTodo.ts
@@ -0,0 +1,20 @@
+import { Page, Locator, expect } from '@playwright/test';
+import { BasePage } from '../BasePage';
+import { LocatorMaster} from '../locators/TodoLocators';
+
+export class CheckTodo extends BasePage {
+
+  constructor(page: Page) {
+    super(page);
+  }
+  get url(): string {
+    return 'https://demo.playwright.dev/todomvc';
+  }
+
+  //Action
+  async checkTodo(index: number): Promise<void> {
+    const locator = new LocatorMaster(this.page);
+    // Check first todo
+    await locator.todoCheckbox(index).check();
+  }
+}
